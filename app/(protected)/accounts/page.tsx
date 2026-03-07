af1bc0f82dacc6a3e2fb2de0bc5d13343c343840
Benedetto Marotta <benedettomarotta@hotmail.it>
2026-03-07 23:33:04 +0100

    fix: remove centering styles from BankList container for improved layout consistency

--- app/(protected)/accounts/page.tsx
+++ app/(protected)/accounts/page.tsx
@@ -4,7 +4,7 @@ export default function Link() {
     return (
         <main className="flex h-full w-full flex-col gap-4">
             <h1 className="text-2xl font-bold">Link</h1>
-            <div className="flex h-full w-full flex-col items-center justify-center">
+            <div className="flex h-full w-full flex-col">
                 <BankList />
             </div>
         </main>
